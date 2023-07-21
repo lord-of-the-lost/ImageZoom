32fd5affece8ff4c8d8e28214cad767b75dca4d7
Nikolay Ignatov <ignatov.99@yandex.ru>
2023-07-21 20:26:04 +0300

    add image with zoom

--- ImageZoom/ViewController.swift
+++ ImageZoom/ViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    private let defaultImageHeight: CGFloat = 270
 
+    private lazy var scrollView: UIScrollView = {
+        var scrollView = UIScrollView()
+        scrollView.contentSize = CGSize(width: view.bounds.width, height: 1500)
+        scrollView.contentInsetAdjustmentBehavior = .never
+        scrollView.delegate = self
+        scrollView.backgroundColor = .white
+        return scrollView
+    }()
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: defaultImageHeight))
+        imageView.image = UIImage(named: "image")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view = scrollView
+        scrollView.addSubview(imageView)
     }
-
-
 }
 
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offset = scrollView.contentOffset.y
+        if offset <= 0 {
+            imageView.frame.origin.y = offset
+            imageView.frame.size.height = defaultImageHeight - offset
+        }
+        scrollView.verticalScrollIndicatorInsets.top = defaultImageHeight - offset - view.safeAreaInsets.top
+    }
+}
